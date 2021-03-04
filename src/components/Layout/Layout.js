c3cbe61f83dd00db98752bb3c28a945458db8c83
Rajasekar <rpalanisamy@ztech.io>
2021-03-04 18:51:21 +0530

    added side-drawer and responsive style for mobiles

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from '../Layout/Layout.css';
 import Aux from '../../hoc/Aux';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: false
+    }
+
+    closeSideDrawer = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    openSideDrawer = () => {
+        this.setState((prevState)=> {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar showOrHideDrawer = {this.openSideDrawer}/>
+                <SideDrawer open = {this.state.showSideDrawer}
+                 closed = {this.closeSideDrawer}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+
+export default Layout;
